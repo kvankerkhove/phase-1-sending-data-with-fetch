14b2d5f080e5502d517248dbffe3f16b92951d28
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-12 18:10:22 -0400

    commit tree 9f19a9e768e0768abdc2c9bdc1cba8542738fe40

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(res => res.json())
+//   .then(object => console.log(object))
+
+  
+
+  
+  const submitData = (username, useremail) => {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: useremail,
+        })
+    }
+      return fetch("http://localhost:3000/users", configurationObject)
+      .then(res => res.json())
+      .then(object => document.querySelector("body").append(`${object.id}`))
+      .catch(error => document.querySelector("body").append(error.message))
+      
+  }
+
+  submitData('danny', '999@gmail.com')
+
+  
